b070d4b111d56a277b2cb6a4c8db71ade57a32d5
likaicheng <likaicheng>
2022-01-18 11:29:44 +0800

    修改websocket

--- src/main/java/com/likc/websocket/WebSocketServer.java
+++ src/main/java/com/likc/websocket/WebSocketServer.java
@@ -26,7 +26,6 @@ public class WebSocketServer {
     @Autowired
     private ObjectMapper objectMapper;
 
-
     // 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的
     private static int onlineCount = 0;
     // concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象
@@ -140,8 +139,8 @@ public class WebSocketServer {
      * 群发消息
      */
     public static void sendAll(String message) throws IOException {
-        for (Map.Entry<String, WebSocketServer> sessionEntry : webSocketMap.entrySet()) {
-            sessionEntry.getValue().sendMessage(message);
+        for (Map.Entry<String, WebSocketServer> webSocketServer : webSocketMap.entrySet()) {
+            webSocketServer.getValue().sendMessage(message);
         }
     }
 
