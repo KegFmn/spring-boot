c7345722e1a08ecb9e65293328d9bf75a83f8d1f
likaicheng <likaicheng>
2022-02-16 11:23:34 +0800

    解决No SecurityManager accessible to the calling code报错 druid与shiro拦截顺序问题

--- src/main/java/com/likc/config/ShiroConfig.java
+++ src/main/java/com/likc/config/ShiroConfig.java
@@ -8,6 +8,7 @@ import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.util.ThreadContext;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.crazycake.shiro.RedisCacheManager;
@@ -60,6 +61,7 @@ public class ShiroConfig {
         securityManager.setCacheManager(redisCacheManager);
 
         // other stuff...
+        ThreadContext.bind(securityManager);
 
         return securityManager;
     }
@@ -68,7 +70,8 @@ public class ShiroConfig {
     public ShiroFilterChainDefinition shiroFilterChainDefinition() {
         DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
         Map<String, String> filterMap = new LinkedHashMap<>();
-        filterMap.put("/**", "jwt"); // 主要通过注解方式校验权限
+        // 主要通过注解方式校验权限
+        filterMap.put("/**", "jwt");
         chainDefinition.addPathDefinitions(filterMap);
         return chainDefinition;
     }
