cdaaa3175765bef14baab8b6e0f54e29ca12a457
KegFmn <1165624857@qq.com>
2022-03-17 01:06:28 +0800

    新增登录逻辑

---
+++ src/main/java/com/likc/controller/AccountController.java
@@ -0,0 +1,58 @@
+package com.likc.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.likc.common.dto.LoginDto;
+import com.likc.common.lang.Result;
+import com.likc.po.User;
+import com.likc.service.UserService;
+import com.likc.util.JwtUtils;
+import com.likc.vo.LoginVo;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @CrossOrigin
+    @PostMapping("/login")
+    public Result<LoginVo> login(@Validated @RequestBody LoginDto loginDto, HttpServletResponse response){
+
+        User user = userService.getOne(new QueryWrapper<User>().eq("user_name", loginDto.getUserName()));
+        Assert.notNull(user,"用户不存在");
+
+        if (!user.getPassWord().equals(DigestUtils.md5Hex(loginDto.getPassWord()))){
+            return new Result<>(400, "密码不正确");
+        }
+
+        String jwt = jwtUtils.generateToken(user.getId());
+        response.setHeader("Authorization", jwt);
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+
+        LoginVo loginVo = new LoginVo();
+        BeanUtils.copyProperties(user, loginVo);
+
+        return new Result<>(200, "请求成功", loginVo);
+    }
+
+    @RequiresAuthentication
+    @GetMapping("/logout")
+    public Result<Object> logout(){
+        SecurityUtils.getSubject().logout();
+        return new Result<>(200, "请求成功");
+    }
+
+}
---
+++ src/main/java/com/likc/vo/LoginVo.java
@@ -0,0 +1,23 @@
+package com.likc.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author likc
+ * @date 2022/3/16
+ * @description
+ */
+
+@Getter
+@Setter
+public class LoginVo {
+
+    private Long id;
+
+    private String userName;
+
+    private String avatar;
+
+    private String email;
+}
